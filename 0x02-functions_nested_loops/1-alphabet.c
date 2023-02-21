0437b4600e2e0bc145fbd74ea7779042192d0cf2
Uange1 <aange1203@gmail.com>
2023-02-21 15:24:03 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet .print all alphabet in lowecase
+ */
+
+void print_alphabet(void)
+{
+	char letter,
+	for(letter = 'a'; letter<='z';letter++)
+	   	putchar(letter);
+
